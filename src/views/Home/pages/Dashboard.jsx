5c4a8b5340d003a89cce6b3b41c2cb6135a2760d
Bryce Mercines <nplixel@gmail.com>
2019-11-10 23:21:42 +0800

    completed ui for info card

--- src/views/Home/pages/Dashboard.jsx
+++ src/views/Home/pages/Dashboard.jsx
@@ -36,13 +36,28 @@ function Dashboard(props) {
           <p className="hazard-tag"><i className="fa fa-bullseye"></i> Earthquake</p>
           <span className="time-tag"><i className="fa fa-clock-o"></i> 2 hours ago</span>
           <div className="info-body">
+
+            <p className="detail-text"><i className="fa fa-chevron-right"></i> Report Details</p>
             <div className="detail-card">
               complete na yung api, and chatbot deployed na rin sa heroku, putting my full power on the front end now, haha
              </div>
             <hr />
+
+            <center>
+              <p className="detail-text"><i className="fa fa-bars"></i> Supporting Information</p>
+            </center>
+            <hr />
+
+            <p className="detail-text"><i className="fa fa-map-marker"></i> LandMarks</p>
+            <div className="support-card">
+              Pasay City. Philippines.
+             </div>
+
+
           </div>
           <div className="info-footer">
-            <button className="test-btn"><i className="fa fa-check" /> Verify</button>
+            <button className="locate"><i className="fa fa-crosshairs" /> Locate</button>
+            <button className="verify"><i className="fa fa-check" /> Verify</button>
           </div>
         </div>
       </div>
