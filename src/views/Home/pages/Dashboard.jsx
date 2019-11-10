f5ae7c6f3bb0c4a612951a22abde61eca0bc38cc
Bryce Mercines <nplixel@gmail.com>
2019-11-11 01:33:53 +0800

    basic rendering , and aded maps

--- src/views/Home/pages/Dashboard.jsx
+++ src/views/Home/pages/Dashboard.jsx
@@ -18,13 +18,25 @@ function Dashboard(props) {
   }, [])
 
 
+  if (document.contains(document.getElementById('renderMap'))) {
+    setTimeout(() => {
+      document.getElementById("renderMap").click();
+    }, 1000)
+  }
+
+
   return (
     <div className="dashboard">
       <div className="feed">
         {
           Array.from(Array(7)).map((i, index) => (
             <div className="card">
+              <span className="tag"><i className="fa fa-fire"></i> FIRE</span>
+              <span className="time-tag"><i className="fa fa-clock-o"></i> 2 hours ago</span>
 
+              <p className="detail">
+                complete na yung api, and chatbot deployed na rin sa heroku, putting my full power on complete na yung api, and chatbot deployed na rin sa heroku, putting my full power on
+              the front end now, haha</p>
             </div>
           ))
         }
@@ -60,6 +72,10 @@ function Dashboard(props) {
             <button className="verify"><i className="fa fa-check" /> Verify</button>
           </div>
         </div>
+
+        <div id="googleMap"></div>
+
+
       </div>
     </div>
   )
