f01593825a690947d40ee97e748e1c281c69a10f
Bryce Mercines <nplixel@gmail.com>
2019-11-12 16:48:28 +0800

    dashboard functions complete

---
+++ public/script.js
@@ -0,0 +1,449 @@
+
+function renderGMap() {
+
+  let lat, long;
+
+  if (localStorage.lat && localStorage.long) {
+    lat = localStorage.lat;
+    long = localStorage.long;
+  } else {
+    lat = 14.508822;
+    long = 121.001707;
+  }
+
+  let xcenter = new google.maps.LatLng(lat, long);
+
+  var mapOptions = {
+    center: xcenter,
+    zoom: 10, scrollwheel: false, draggable: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+      {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#1673cb"
+          }
+        ]
+      },
+      {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "hue": "#0066ff"
+          },
+          {
+            "weight": 1.9
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "saturation": 86
+          },
+          {
+            "hue": "#0077ff"
+          },
+          {
+            "weight": 0.8
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "saturation": "18"
+          },
+          {
+            "lightness": "24"
+          },
+          {
+            "gamma": "1.84"
+          },
+          {
+            "weight": "6.56"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "saturation": "-29"
+          },
+          {
+            "lightness": "35"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c7e2ff"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#bcddff"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#4797e0"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "hue": "#0077ff"
+          },
+          {
+            "saturation": -7
+          },
+          {
+            "lightness": 24
+          }
+        ]
+      },
+      {
+        "featureType": "poi.attraction",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "saturation": "-2"
+          },
+          {
+            "lightness": "3"
+          },
+          {
+            "gamma": "2.10"
+          },
+          {
+            "visibility": "simplified"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#209ee1"
+          },
+          {
+            "lightness": 49
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#5fb3ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#3ea3ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "saturation": 58
+          },
+          {
+            "hue": "#006eff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "saturation": "-13"
+          },
+          {
+            "lightness": "66"
+          },
+          {
+            "color": "#ee952c"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+          {
+            "color": "#ebf4ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "saturation": "52"
+          },
+          {
+            "lightness": "5"
+          },
+          {
+            "gamma": "4.11"
+          },
+          {
+            "weight": "1.59"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "lightness": "10"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "saturation": "35"
+          },
+          {
+            "lightness": "49"
+          },
+          {
+            "gamma": "5.79"
+          },
+          {
+            "weight": "5.45"
+          },
+          {
+            "color": "#7245d5"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ebf4ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "color": "#93c8ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "saturation": "20"
+          },
+          {
+            "color": "#7b4646"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ff6100"
+          },
+          {
+            "lightness": "-62"
+          },
+          {
+            "gamma": "2.01"
+          },
+          {
+            "weight": "8.64"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "saturation": "20"
+          },
+          {
+            "visibility": "on"
+          },
+          {
+            "gamma": "2.02"
+          },
+          {
+            "weight": "3.96"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "saturation": 100
+          },
+          {
+            "gamma": 0.82
+          },
+          {
+            "hue": "#0088ff"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "saturation": "-2"
+          },
+          {
+            "lightness": "-81"
+          },
+          {
+            "gamma": "1.50"
+          }
+        ]
+      }
+    ]
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: xcenter,
+  });
+  marker.setMap(map);
+}
+
+const el = (element) => {
+  return document.getElementById(element);
+}
+
+const cardSelect = (uid) => {
+  let dataset = JSON.parse(localStorage.incidents);
+  let reportData = dataset.find(item => { return item.uid === uid })
+
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${reportData.location.lat},${reportData.location.long}&key=AIzaSyD5kFZMwUIUDZ25nTtLx0_0G3x1d2GMiCY`)
+    .then(res => {
+      if (res.data.status === "OK") {
+
+        localStorage.lat = reportData.location.lat;
+        localStorage.long = reportData.location.long;
+
+        localStorage.uid = uid;
+        localStorage.psid = reportData.id;
+
+        renderGMap();
+
+        setTimeout(() => {
+          document.body.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }, 1000)
+
+        setTimeout(() => {
+          if (document.getElementsByClassName('gm-control-active')[1]) {
+            document.getElementsByClassName('gm-control-active')[1].click();
+            document.getElementsByClassName('gm-control-active')[1].click();
+            document.getElementsByClassName('gm-control-active')[1].click();
+            document.getElementsByClassName('gm-control-active')[1].click();
+          }
+        }, 2000)
+
+        el('location').innerHTML = res.data.results[0].formatted_address;
+        if (reportData.type === 'others') {
+          el('hazard-type').innerHTML = 'accident'
+        } else {
+          el('hazard-type').innerHTML = reportData.type;
+        }
+
+        el('report-details').innerHTML = reportData.details;
+        el('geolocation').innerHTML = '';
+
+        let locationCount = 1;
+
+        res.data.results.forEach(location => {
+          el('geolocation').innerHTML += `
+           <p class="detail-text"><i class="fa fa-map-marker"></i> ${locationCount}.) ${location.geometry.location_type.split('_').join(" ")}</p>
+           <div class="support-card">
+               ${location.formatted_address}
+               <hr>
+               <strong>Coordinates</strong>
+               <ul class="coordinates">
+                  <li>Latitude: <span>${location.geometry.location.lat}</span></li>
+                  <li>Longtitude: <span>${location.geometry.location.lng}</span></li>
+               </ul>
+
+           </div>
+           `
+          locationCount++;
+        });
+
+        el('infoCard').style.display = 'block';
+
+      }
+    })
+}
+
--- src/views/Home/pages/Dashboard.jsx
+++ src/views/Home/pages/Dashboard.jsx
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react'
 import PerfectScrollbar from 'perfect-scrollbar'
 import loading from '../../../assets/img/loading.gif'
 import axios from 'axios';
+import moment from 'moment';
+import InfoCard from './InfoCard'
 
 import io from 'socket.io-client';
 
 
 const socket = io('https://sagipinas.herokuapp.com');
 
-socket.on("report", (data) => {
-  console.log(data);
-})
+
 
 
 function Dashboard(props) {
@@ -21,12 +21,15 @@ function Dashboard(props) {
     document.getElementById('feed').innerHTML = '';
     axios.get('https://sagipinas.herokuapp.com/incidents')
       .then(res => {
+        localStorage.incidents = JSON.stringify(res.data)
+
         res.data.reverse().forEach(feed => {
           if (feed.status === 'unverified') {
             document.getElementById('feed').innerHTML += `
-            <div class="card">
-            <span class="tag"><i class="fa fa-warning"></i> ${feed.type === 'others' ? "Accident" : feed.type}</span>
-            <span class="time-tag"><i className="fa fa-clock-o"></i> 2 hours ago</span>
+            <div class="card" onclick="cardSelect('${feed.uid}')" id="${feed.uid}">
+            <span class="pulseDot"></span>
+          <span class="tag"><i class="fa fa-warning"></i> ${feed.type === 'others' ? "Accident" : feed.type}</span>
+            <span class="time-tag"><i className="fa fa-clock-o"></i> ${moment(feed.timestamp).fromNow()}</span>
             <p class="detail">
               ${feed.details}
              </p>
@@ -35,14 +38,18 @@ function Dashboard(props) {
           }
         })
       })
+
   }
+
+  socket.on("report", (data) => {
+    loadFeed();
+  })
 
   useEffect(() => {
     new PerfectScrollbar('.feed')
     new PerfectScrollbar('.info-body')
 
     loadFeed();
-
 
     // if (document.contains(document.getElementById('renderMap'))) {
     //   setTimeout(() => {
@@ -59,45 +66,10 @@ function Dashboard(props) {
       </div>
 
       <div className="content">
-        <div className="info-card animated slideInUp fastest" id="infoCard">
-          <h3><i className="fa fa-map-marker marker"></i> Manila, Philippines</h3>
-          <p className="hazard-tag"><i className="fa fa-bullseye"></i> Earthquake</p>
-          <span className="time-tag"><i className="fa fa-clock-o"></i> 2 hours ago</span>
-          <div className="info-body">
-
-            <p className="detail-text"><i className="fa fa-chevron-right"></i> Report Details</p>
-            <div className="detail-card">
-              complete na yung api, and chatbot deployed na rin sa heroku, putting my full power on the front end now, haha
-             </div>
-            <hr />
-
-            <center>
-              <p className="detail-text"><i className="fa fa-bars"></i> Supporting Information</p>
-            </center>
-            <hr />
-
-            <p className="detail-text"><i className="fa fa-map-marker"></i> LandMarks</p>
-            <div className="support-card">
-              Pasay City. Philippines.
-             </div>
-
-
-          </div>
-          <div className="info-footer">
-            <button className="locate"><i className="fa fa-crosshairs" /> Locate</button>
-            <button className="verify"
-              onClick={() => {
-                socket.emit("verifyReport", { id: 2695506260470251, uid: "demo1" })
-              }}
-            ><i className="fa fa-check" /> Verify</button>
-          </div>
-        </div>
-
+        <InfoCard />
         <div id="googleMap">
           <img src={loading} className="map-loading" alt="loading" />
         </div>
-
-
       </div>
     </div>
   )
