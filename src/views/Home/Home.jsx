a33fb9061c2a9f74cc9b1490eb2555461f82474c
Bryce Mercines <nplixel@gmail.com>
2019-11-24 21:05:57 +0800

    saving progress, converting app to landing page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,21 +1,85 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-
-import Sidebar from '../../components/Sidebar'
-import Dashboard from './pages/Dashboard'
-import Incidents from './pages/Incidents'
-import News from './pages/News'
-import Settings from './pages/Settings'
+import logo from '../../assets/img/logo.png';
+import app1 from '../../assets/img/app1.png';
+import app2 from '../../assets/img/app2.png';
 
 function Home(props) {
   return (
     <div className="content-container">
-      <Sidebar />
+      <div className="gradient-half">
+      </div>
 
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/incidents" component={Incidents} />
-      <Route path="/news" component={News} />
-      <Route path="/settings" component={Settings} />
+      <div className="page">
+        <div className="row my-4">
+
+          <div className="col-md-5">
+            <h1>A disaster risk response platform for everyone</h1>
+            <p><span className="text-warning">SagiPinas 1.0</span> is under development.</p>
+            <button class="btn-hover color-1 join-btn"><i className="fa fa-github mr-1"></i> JOIN THE PROJECT</button>
+          </div>
+
+          <div className="col-md-6">
+            <div className="preview">
+              <img src={app1} className="app fade-in-bottom" alt="app" />
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="logo-banner p-5">
+        <h4><img src={logo} className="logo" alt="sagip-logo" />
+          Sagi<span>Pin</span>as
+        </h4>
+      </div>
+
+      <button class="btn-hover color-9 btn-learn">LEARN MORE</button>
+
+
+
+      <div className="mobile animated fade-in-bottom"></div>
+
+      <div className="body-page">
+
+        <div className="row mb-4">
+          <div className="col-md-7">
+            <img src={app2} className="app" alt="img-app" />
+          </div>
+          <div className="col-md-4">
+            <h2>About the Project</h2>
+            <div className="divider"></div>
+            <p className="text-muted">The project aims to take leverage of technology to help victims of accidents
+              and evironmental disasters to call out for help  without relying on full internet connection, which gives
+              better changes of locating people who are in need of aid when disaster strikes.
+          </p>
+
+            <p className="text-muted">
+              This is initiated in response to the increasing number of environmental disasters occuring in the Philippines,
+              causing hundred of casualties and putting hundreds of lives at risk.
+          </p>
+
+            <p className="built-by animated fadeIn">BUILT BY:</p>
+            <div className="contributors animated fadeIn">
+              <img src="https://avatars0.githubusercontent.com/u/22174665?s=460&v=4" className="contributor-avatar" alt="avatar" />
+              <img src="https://avatars3.githubusercontent.com/u/21337635?s=460&v=4" className="contributor-avatar" alt="avatar" />
+              <img src="https://avatars0.githubusercontent.com/u/29462205?s=460&v=4" className="contributor-avatar" alt="avatar" />
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
