7b4ff5f1ec0177d85431055230583bf5b270d227
Bryce Mercines <nplixel@gmail.com>
2020-07-12 15:05:52 +0800

    added typescript config, and convert files to TS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `An open disaster risk response platform`,
+    description: ``,
+    author: `@quadroloop`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <SEO title="SagiPinas" />
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `700px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
