94135a09779e02f1db9ac1f56d3727be4d094b66
Bryce Mercines <nplixel@gmail.com>
2020-07-27 20:06:28 +0800

    saving progress on data fetching

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
     description: `This is a simple website`,
     author: `@quadroloop`,
   },
+
+
+
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -27,6 +30,30 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `testdataX`,
+        fieldName: `testData`,
+        url: `https://swapi.dev/api/people/`
+      },
+    },
+
+    {
+      resolve: "gatsby-source-custom-api",
+      options: {
+        url: "https://swapi.dev/api/people",
+        rootKey: "results",
+        schemas: {
+          results: `
+             name: String
+            `
+        }
+      }
+    },
+
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import axios from 'axios'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+
+  const [data, setData] = useState<any>([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log(res.data.results)
+        setData(res.data.results)
+      })
+  }, [])
+
+  return (<Layout>
     <SEO title="SagiPinas" />
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `700px`, marginBottom: `1.45rem` }}>
@@ -15,7 +27,19 @@ const IndexPage = () => (
     <p>I wonder how to edit this is wierd and we don't know how it really works</p>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <ul>
+      {data && (
+        data.map((item, index) => {
+          return (
+            <li key={index}>{item.name}</li>
+          )
+        })
+      )}
+    </ul>
   </Layout>
-)
+  )
+
+}
 
 export default IndexPage
