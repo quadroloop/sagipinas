57d53d75bfd0e6a3d9b01f8e9530106f6931f04a
Bryce Mercines <nplixel@gmail.com>
2020-09-09 20:11:56 +0800

    minor layout adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx("col col--4", styles.feature)}>
+    <div className={clsx("col col--4 text--center", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
